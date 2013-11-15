84309b89aeb1eedbbe4b117e5edeca2dc5b7a4e3
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-15 15:18:31 -0600

    ajax-ified

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,5 @@
 get '/' do
+  session.clear #can't leave this in for non js versions
   erb :index
 end
 
@@ -6,9 +7,12 @@ get '/offering' do
   offering = Offering.new(params[:offering])
   if offering.pleasing?
     session[:accepted] = true
+    accepted = {'accepted' => 'true'}.to_json
   else
     session[:accepted] = false
+    accepted = {'accepted' => 'false'}.to_json
   end
-  redirect '/'
+  # redirect '/'
+  return accepted
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,44 @@
 $(document).ready(function() {
 
-  var greeting = "<section class='greeting message'><h2>Hello, human.</h2><h3>What pitiful offering have you brought to place before the guardians of the green glass door?</h2></section>"
-  var accept_notice = "<section class='accept-notice message'><p>Well done!  Your offering has pleased the guardians of the green glass door.</p><p>You may enter.</p></section>"
-  var reject_notice = "<section class='reject-notice message'><p>Your pathetic offering has been rejected.</p><p>It can't come in, and neither can you!</p></section>"
-  var try_again? = "<section class='try-again message'><p>Would you like to try again, puling mortal?</p><a href='/retry'><button>Yes</button></a><a href='/leave'><button>No</button></a></section>"
-  var very_well = "<p class='very-well message'>Very well.  What have you brought this time?</p>"
-  var get_out = "<p class='get-out message'>Fine!<p/>"
-  var no_really = "<p class='no-really message'>LEAVE.</p>"
-  var offer_form = "<form action='/offering' method='get' class='offer-form'><input type='text' name='offering'><input type='submit' value='Make an offering'></form>"
+  $('.hide-on-start').hide()
+  $('.greeting').fadeIn(2000)
+  $('.offer-form').fadeIn(2000)
+
+  $('.offer-form').on('submit', function(event) {
+    event.preventDefault();
+    
+    var offering = $(this).serialize();
+    
+    $('.message').fadeOut(1000);
+    $('.offer-form').fadeOut(1000);
+    $('.sweet-form').prop('value', '');
+    
+    $.get('/offering', offering, function(verdict) {
+      if (verdict.accepted === 'true') {
+        $('.message').fadeOut(1000);
+        $('.accept-notice').delay(1000).fadeIn(2000);
+      } else {
+        $('.message').fadeOut(1000);
+        $('.reject-notice1').delay(1000).fadeIn(2000);
+        $('.reject-notice2').delay(3000).fadeIn(2000);
+        $('.try-again').delay(5000).fadeIn(2000);
+        $('.yes').on('click', function() {
+          $('.message').fadeOut(1000);
+          $('.very-well').delay(1500).fadeIn(2000);
+          $('.offer-form').delay(1500).fadeIn(2000);
+        });
+        $('.no').on('click', function(event) {
+          $('.message').fadeOut(1000);
+          $('.get-out').delay(1500).fadeIn(2000);
+          $('.message').delay(1000).fadeOut(1000)
+          $('.no-really').delay(4500).fadeIn(2000);
+          // $('.message').delay(2000).fadeOut(1000)
+          // $('.greeting').delay(5000).fadeIn(2000)
+          // $('.offer-form').delay(6000).fadeIn(2000)
+        });
+      };
+    }, 'json');
+  });
+
 
 });
